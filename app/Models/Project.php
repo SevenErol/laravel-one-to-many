5e5882d2fca378fb0e1a340da3d0c6fe42960288
SevenErol <lorenzo.maggi1998@gmail.com>
2023-01-16 15:47:44 +0100

    models and foreign key

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,9 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+use App\Models\Type;
+
 class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'cover_image', 'description', 'date'];
+    protected $fillable = ['title', 'cover_image', 'description', 'date', 'type_id'];
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -5,7 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+use App\Models\Project;
+
 class Type extends Model
 {
     use HasFactory;
+
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
 }
