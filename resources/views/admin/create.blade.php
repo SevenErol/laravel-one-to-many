6215918dba27f88c520593c1fdaeb04f8c19e976
SevenErol <lorenzo.maggi1998@gmail.com>
2023-01-16 19:14:24 +0100

    fixed value selected in create view

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -49,7 +49,7 @@
                 <option selected>Select one</option>
 
                 @foreach ($types as $type )
-                <option value="{{$type->id}}" {{ old('type_id') ? 'selected' : '' }}>{{$type->name}}</option>
+                <option value="{{$type->id}}" {{ $type->id == old('type_id') ? 'selected' : ''  }}>{{$type->name}}</option>
                 @endforeach
 
             </select>
